f8d1eeadf5c6285ccc10a59963f8a7fc806910b1
Melody <melodysamuelcodes@gmail.com>
2025-01-01 19:12:54 +0100

    # add new task

--- src/index.ts
+++ src/index.ts
@@ -6,3 +6,20 @@ interface Task {
     createdAt: string;
     updatedAt: string;
 } 
+
+import { readTasks, writeTasks } from '../src/utils';
+
+// Add a new task 
+const addTask = (description: string): void => {
+    const tasks = readTasks();
+    const newTask: Task = {
+        id: tasks.length + 1,
+        description,
+        status: 'todo',
+        createdAt: new Date().toISOString(),
+        updatedAt: new Date().toISOString(),
+    }
+    tasks.push(newTask);
+    writeTasks(tasks);
+    console.log(`Task added succesfully (ID: ${newTask. id})`);
+};
--- src/utils.ts
+++ src/utils.ts
@@ -1,9 +1,16 @@
+import { table } from 'console';
 import fs from 'fs';
 
 // Read tasks from the json file 
 export const readTasks = (): Task[] => {
     try {
         const data = fs.readFileSync('tasks.json', 'utf-8');
-        return JSON.parse(data || )
+        return JSON.parse(data || '[]') as Task[];
+    } catch (err) {
+        return []
     }
+}
+
+export const writeTasks = (tasks: Task[]): void => {
+    fs.writeFileSync('tasks.json', JSON.stringify(tasks, null, 2), 'utf-8' )
 }
